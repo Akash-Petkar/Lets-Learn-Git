97ad4dc4c704cd606dc78293fb8f9b7f9e3e1d0b
Akash Petkar <akashpetkar04@gmail.com>
2022-06-29 18:59:00 +0530

    multiply b value changed to 8

--- Multiply.java
+++ Multiply.java
@@ -3,7 +3,7 @@ public class Multiply
     public static void main(String args[])
     {
         int a=2;
-        int b=2;
+        int b=8;
 
         System.out.println("The Product is "+a*b);
     }
